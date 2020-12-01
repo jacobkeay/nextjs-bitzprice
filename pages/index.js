a948133d8a65cf969632ec7d5c46bbda10dcb02a
jacobkeay <jacobkeay@outlook.com>
2020-12-01 23:49:42 +0000

    Changed paragraph to indicate update

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ const Index = props => (
     <div>
       <h1>Welcome to BitzPrice</h1>
       <p>Check current Bitcoin rate</p>
+      <p>There has been an update!</p>
       <Prices bpi={props.bpi} />
     </div>
   </Layout>
